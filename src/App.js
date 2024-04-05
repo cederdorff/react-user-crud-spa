62dc9ae275ce2964ad91e201660530c6d3c53aea
RACE (Rasmus Cederdorff) <rasmus@cederdorff.com>
2024-04-05 10:51:39 +0200

    Refactor App.js and HomePage.js components

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
 import UpdatePage from "./pages/UpdatePage";
 function App() {
-    return (
-        <main>
-            <Nav />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/create" element={<CreatePage />} />
-                <Route path="/users/:id" element={<UserPage />} />
-                <Route path="/update/:id" element={<UpdatePage />} />
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-        </main>
-    );
+  return (
+    <main>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/users/:id" element={<UserPage />} />
+        <Route path="/update/:id" element={<UpdatePage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,30 +2,32 @@ import { useEffect, useState } from "react";
 import User from "../components/User";
 
 export default function HomePage() {
-    const [users, setUsers] = useState([]); // state to handle the data (users)
-    // users: name of the state
-    // setUsers: name of the function to set the state
+  const [users, setUsers] = useState([]); // state to handle the data (users)
+  // users: name of the state
+  // setUsers: name of the function to set the state
 
-    //the side effect - fetch users
-    useEffect(() => {
-        getData();
-    }, []); // <--- "[]" VERY IMPORTANT!!!
+  //the side effect - fetch users
+  useEffect(() => {
+    getData();
+  }, []); // <--- "[]" VERY IMPORTANT!!!
 
-    async function getData() {
-        const response = await fetch("https://race-crud-rest-default-rtdb.firebaseio.com/users.json"); // read all users from firebase
-        const data = await response.json();
-        const array = Object.keys(data).map(key => ({ id: key, ...data[key] })); // from object to array
-        setUsers(array); // set the state with fetched data
-    }
+  async function getData() {
+    const response = await fetch(
+      "https://race-rest-default-rtdb.firebaseio.com/users.json"
+    ); // read all users from firebase
+    const data = await response.json();
+    const array = Object.keys(data).map(key => ({ id: key, ...data[key] })); // from object to array
+    setUsers(array); // set the state with fetched data
+  }
 
-    return (
-        <section className="page">
-            <h1>Home Page</h1>
-            <section className="grid-container">
-                {users.map(userObj => (
-                    <User user={userObj} key={userObj.id} />
-                ))}
-            </section>
-        </section>
-    );
+  return (
+    <section className="page">
+      <h1>Users</h1>
+      <section className="grid">
+        {users.map(userObj => (
+          <User user={userObj} key={userObj.id} />
+        ))}
+      </section>
+    </section>
+  );
 }
